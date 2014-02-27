a1d44b0730687d9ab3d4859705dce3df788300df
ndtgit01 <ndtgit01@hotmail.com>
2014-02-27 23:39:09 +0700

    Add moveStopped for Turn Left or Right

--- Assets/Scripts/Games/PlayerPhysics.cs
+++ Assets/Scripts/Games/PlayerPhysics.cs
@@ -8,10 +8,13 @@ public class PlayerPhysics : MonoBehaviour
 	private BoxCollider collider;
 	private Vector3 size;
 	private Vector3 center;
-	private float dropDistancePerFrame = 0.005f;
+	private float moveStep = 0.005f;
+
 	[HideInInspector]
-	public bool
-		grounded;
+	public bool grounded;
+	[HideInInspector]
+	public bool movementStopped;
+
 	Ray ray;
 	RaycastHit hit;
 	
@@ -25,31 +28,57 @@ public class PlayerPhysics : MonoBehaviour
 
 	public void Move (Vector2 moveAmount)
 	{
-		float deltaX = moveAmount.x;
-		float deltaY = moveAmount.y;
+		float newX = moveAmount.x;
+		float newY = moveAmount.y;
 		Vector2 position = transform.position;
+
+
 		grounded = false;
 		for (int i = 0; i < 3; i++) {
-			float dir = Mathf.Sign (deltaY);
+			float dir = Mathf.Sign (newY);
 			float xRay = (position.x + center.x - size.x / 2) + size.x / 2 * i;
 			float yRay = position.y + center.y + size.y / 2 * dir; 
 			
 			ray = new Ray (new Vector2 (xRay, yRay), new Vector2 (0, dir));
 			MyLog.DrawRay(ray.origin,ray.direction);
-			if (Physics.Raycast (ray, out hit, Mathf.Abs (deltaY), collisionMask)) {
+			if (Physics.Raycast (ray, out hit, Mathf.Abs (newY)+moveStep, collisionMask)) {
 				// Distance Player vs Ground
 				float distance = Vector3.Distance (ray.origin, hit.point);
 				// Stop player on ground
-				if (distance > dropDistancePerFrame) {
-					deltaY = -distance + dropDistancePerFrame;
+				if (distance > moveStep) {
+					newY = -distance + moveStep;
 				} else {
-					deltaY = 0;
+					newY = 0;
 				}
 				grounded = true;
 				break;
 			}
 		}
-		Vector2 finalMoveAmount = new Vector2 (deltaX, deltaY);
+
+		movementStopped = false;
+		for (int i = 0; i < 3; i++) {
+			float dir = Mathf.Sign (newX);
+
+			float xRay = position.x + center.x + size.x / 2 * dir; 
+			float yRay =  (position.y + center.y - size.y / 2) + size.y / 2 * i; 
+			
+			ray = new Ray (new Vector2 (xRay, yRay), new Vector2 (dir, 0));
+			MyLog.DrawRay(ray.origin,ray.direction);
+			if (Physics.Raycast (ray, out hit, Mathf.Abs (newX)+ moveStep, collisionMask)) {
+				// Distance Player vs Ground
+				float distance = Vector3.Distance (ray.origin, hit.point);
+				// Stop player on ground
+				if (distance > moveStep) {
+					newX = -distance + moveStep;
+				} else {
+					newX = 0;
+				}
+				movementStopped = true;
+				break;
+			}
+		}
+
+		Vector2 finalMoveAmount = new Vector2 (newX, newY);
 		transform.Translate (finalMoveAmount);
 	}
 }
